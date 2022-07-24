87f997327584ebf6b4524f5fe3992147a9a8fd1a
Denis Petrovskii <denpetrovsky369@gmail.com>
2022-07-24 14:44:57 +0300

    updated maven.yml

--- src/main/java/ru/netology/stats/StatsService2.java
+++ src/main/java/ru/netology/stats/StatsService2.java
@@ -7,7 +7,7 @@ public class StatsService2 {
      * @param incomes — array of incomes
      * @return — index of first max value
      */
-    publiclong findMax(long[] incomes) {
+    public long findMax(long[] incomes) {
         long current_max_index = 0;
         long current_max = incomes[0];
         for (long income : incomes)
